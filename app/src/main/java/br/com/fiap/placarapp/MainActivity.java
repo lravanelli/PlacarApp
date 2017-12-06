f351f56182f5f4b1c91a6f5a4a2170967fe2273f
lravanelli <ravanelli@gmail.com>
2017-12-06 19:30:05 -0200

    teste

--- app/src/main/java/br/com/fiap/placarapp/MainActivity.java
+++ app/src/main/java/br/com/fiap/placarapp/MainActivity.java
@@ -15,7 +15,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        
+
+        etTimeCasa = new EditText(this);
 
         etTimeCasa = (EditText)findViewById(R.id.etTimeCasa);
         etTimeVisitante = (EditText)findViewById(R.id.etTimeVisitante);
